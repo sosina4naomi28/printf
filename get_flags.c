7733460d627319a6a7cee85fc33a0b02b46d756d
zackx-code <74829629+zackx-code@users.noreply.github.com>
2023-03-30 13:17:51 +0200

    Update get_flags.c

--- get_flags.c
+++ get_flags.c
@@ -2,7 +2,7 @@
 
 /**
  * get_flags - Calculates active flags
- * @format: Formatted string in which to print the arguments
+ * @format: Formatt string in which to print the arguments
  * @i: take a parameter.
  * Return: Flags:
  */
